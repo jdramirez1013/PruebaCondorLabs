30fedd13294adce61be938d08b637070e6e67f47
Juan Ramirez <juan-david.ramirez@inetum.world>
2021-11-27 16:52:13 -0500

    first commit

---
+++ app/src/main/java/co/com/jdr/pruebacondorlabs/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package co.com.jdr.pruebacondorlabs
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.timerTask
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Timer().schedule(timerTask {
+
+        }, 2000)
+    }
+}
---
+++ app/src/main/java/co/com/jdr/pruebacondorlabs/core/Extensions.kt
@@ -0,0 +1,10 @@
+package co.com.jdr.pruebacondorlabs.core
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(text: String, length: Int = Toast.LENGTH_SHORT) =
+    this.let { Toast.makeText(it, text, length).show() }
+
+fun Context.getString(resId: Int) =
+    this.let { getString(resId) }
